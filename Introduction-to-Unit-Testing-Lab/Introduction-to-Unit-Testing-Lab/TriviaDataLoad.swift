e85d030ea2ac3c623630069e23fbcc48ec012f11
maitreebain <maitreebain@gmail.com>
2019-12-04 22:04:07 -0500

    all tests finished, working on view joke view controller

--- Introduction-to-Unit-Testing-Lab/Introduction-to-Unit-Testing-Lab/TriviaDataLoad.swift
+++ Introduction-to-Unit-Testing-Lab/Introduction-to-Unit-Testing-Lab/TriviaDataLoad.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+struct TriviaDataLoad: Codable {
+    let results: [TriviaData]
+}
+
+struct TriviaData: Codable {
+    let question: String
+    let correct_answer: String
+    let incorrect_answers: [String]
+}
+
+private enum Rename: String, CodingKey {
+    case question
+    case correct = "correct_answer"
+    case incorrect = "incorrect_answers"
+}
+
+
+
+extension TriviaDataLoad {
+    
+    static func getTrivia() -> [TriviaData] {
+        var trivia = [TriviaData]()
+        
+        guard let fileURL = Bundle.main.url(forResource: "Trivia", withExtension: "json") else {
+            fatalError("no file found")
+        }
+        
+        do {
+            let data = try Data.init(contentsOf: fileURL)
+            
+            let triviaInfo = try JSONDecoder().decode(TriviaDataLoad.self, from: data)
+            trivia = triviaInfo.results
+        }
+        catch {
+            fatalError("no data loaded \(error)")
+        }
+        
+        return trivia
+    }
+    
+}
--- Introduction-to-Unit-Testing-Lab/Introduction-to-Unit-Testing-LabTests/TriviaTests.swift
+++ Introduction-to-Unit-Testing-Lab/Introduction-to-Unit-Testing-LabTests/TriviaTests.swift
@@ -1,5 +1,5 @@
 //
-//  StarWarsTests.swift
+//  TriviaTests.swift
 //  Introduction-to-Unit-Testing-LabTests
 //
 //  Created by Maitree Bain on 12/4/19.
@@ -9,16 +9,14 @@
 import XCTest
 @testable import Introduction_to_Unit_Testing_Lab
 
-class StarWarsTests: XCTestCase {
-
-    let filename = "StarWars"
+class TriviaTests: XCTestCase {
+    
+    let filename = "Trivia"
     let ext = "json"
     
     func dataStorage() {
         let data = Bundle.readRawJsonData(filename: filename, ext: ext)
     }
-    
-    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -26,22 +24,21 @@ class StarWarsTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testStarWarsData() {
+    
+    func testTriviaData() {
         let data = dataStorage
         
         XCTAssertNotNil(data)
     }
     
-    func testSWPopulation() {
+    func testTriviaQuestions() {
         
-        let firstTitle = StarWarsData.getMovieName().first?.title
-        let expectedValue = "A New Hope"
+        let firstQuestion = TriviaDataLoad.getTrivia().first?.question
+        let expectedQuestion = "Who%20wrote%20and%20directed%20the%201986%20film%20%27Platoon%27%3F"
         
-        XCTAssertEqual(firstTitle, expectedValue, "\(firstTitle ?? "") should be \(expectedValue)")
-        
+        XCTAssertEqual(firstQuestion, expectedQuestion, "\(firstQuestion ?? "") should be \(expectedQuestion)")
     }
     
     
-    
+
 }

6a464ac18d58c6259560d6692bf457db2283c458
maitreebain <maitreebain@gmail.com>
2019-12-05 00:29:41 -0500

    trivia vc works working on trivia detail now

--- Introduction-to-Unit-Testing-Lab/Introduction-to-Unit-Testing-Lab/StarWarsViewController.swift
+++ Introduction-to-Unit-Testing-Lab/Introduction-to-Unit-Testing-Lab/StarWarsViewController.swift
@@ -9,22 +9,58 @@
 import UIKit
 
 class StarWarsViewController: UIViewController {
-
+    
+    @IBOutlet weak var SWTableView: UITableView!
+    
+    var starWars = [MovieInfo]() {
+        didSet {
+            SWTableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        SWTableView.dataSource = self
+        loadData()
+        SWTableView.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loadData() {
+        starWars = StarWarsData.getMovieName()
     }
-    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let starWarsVC = segue.destination as? StarWarsDetailsController,
+            let indexPath = SWTableView.indexPathForSelectedRow else {
+                fatalError("no segue found")
+        }
+        let movieSelected = starWars[indexPath.row]
+        
+        starWarsVC.movie = movieSelected
+    }
 
+
+}
+extension StarWarsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension StarWarsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return starWars.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "starWarsCell", for: indexPath)
+        
+        let selectedMovie = starWars[indexPath.row]
+        
+        cell.textLabel?.text = selectedMovie.title
+        
+        return cell
+    }
 }
--- Introduction-to-Unit-Testing-Lab/Introduction-to-Unit-Testing-Lab/TriviaViewController.swift
+++ Introduction-to-Unit-Testing-Lab/Introduction-to-Unit-Testing-Lab/TriviaViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class TriviaViewController: UIViewController {
 
+    @IBOutlet weak var triviaTableView: UITableView!
+    
+    var trivia = [TriviaData]() {
+        didSet {
+            triviaTableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        triviaTableView.dataSource = self
+        loadData()
+        triviaTableView.delegate = self
+    }
+    
+    func loadData() {
+        trivia = TriviaDataLoad.getTrivia()
     }
   
     //replacingoccurances of % from textlabel
 
+}
+
+extension TriviaViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension TriviaViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trivia.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "triviaCell", for: indexPath)
+        
+        let selectedQuestion = trivia[indexPath.row]
+        
+        cell.textLabel?.text = selectedQuestion.question.removingPercentEncoding
+        
+        return cell
+    }
 }
